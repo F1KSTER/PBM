7e994da4fa45fb69511077fb2b9fc31ed36b0cc0
FIKSTER <vilesov.vitala@gmail.com>
2025-11-19 13:01:16 +0300

    Update release workflow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_updater::Builder as UpdaterBuilder;
+
 fn main() {
     tauri::Builder::default()
+        .plugin(UpdaterBuilder::new().build())
         .run(tauri::generate_context!())
-        .expect("error while running Tauri application");
+        .expect("error while running tauri application");
 }
